a641f4058a841edd37c35c7607b1c2a7d96126df
Elvira <elvi_ac94@hotmail.com>
2022-04-06 20:24:58 +0200

    add swagger

--- go.mod
+++ go.mod
@@ -2,4 +2,24 @@ module api-mascota
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/swaggo/swag v1.8.1
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.0.0-20220403103023-749bd193bc2b // indirect
+	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -10,22 +10,36 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Pet model info
+// @Description  Datos de la mascota registrada
+
 type Pet struct {
-	Nombre          string `json:"nombre"`
-	Especie         string `json:"especie"`
-	Genero          string `json:"genero"`
-	Edad            int    `json:"edad"`
-	FechaNacimiento string `json:"fecha nacimiento"`
+	Nombre          string `json:"nombre"`           // Nombre de la mascota
+	Especie         string `json:"especie"`          // Especie de la mascota
+	Genero          string `json:"genero"`           // Genero de la mascota
+	Edad            int    `json:"edad"`             // Edad de la mascota
+	FechaNacimiento string `json:"fecha nacimiento"` // Fecha de nacimiento de la mascota
 }
 
+// KpiMascota model info
+// @Description  Información general
+
 type KpiMascota struct {
-	EspecieMasNumerosa string  `json:"especie mas numerosa"`
-	EdadPromedio       string  `json:"edad promedio perro"`
-	Desviacion         float32 `json:"desviación estándar edad perro"`
+	EspecieMasNumerosa string  `json:"especie mas numerosa"`           // Especie más numerosa
+	EdadPromedio       string  `json:"edad promedio perro"`            // Edad promedio de mis mascotas
+	Desviacion         float32 `json:"desviación estándar edad perro"` // Desviación estándar de las edades de las mascotas
 }
 
 var arrPet []Pet
 
+// createPet godoc
+// @Summary  Crea el registro de una mascota con los datos introducidos
+// @ID       create-pet
+// @Produce  json
+// @Param    pet  body Pet  true  "nombre,especie,genero,edad,fecha de nacimiento"
+// @success 200 {object} Pet
+// @Failure  405  not  allowed
+// @Router   /crearmascota [post]
 func createPet(w http.ResponseWriter, r *http.Request) {
 	var newPet Pet
 	err := json.NewDecoder(r.Body).Decode(&newPet)
@@ -40,6 +54,13 @@ func createPet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// getKpiPet godoc
+// @Summary  Obtiene los datos de kpi mascota
+// @ID       get-kpi-pet
+// @Produce  json
+// @success 200 {object}  KpiMascota
+// @Failure  405  not  allowed
+// @Router   /kpidemascotas [get]
 func getKpiPet(w http.ResponseWriter, r *http.Request) {
 	mascota := &KpiMascota{
 		EspecieMasNumerosa: "Krill Antártico",
@@ -49,13 +70,34 @@ func getKpiPet(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(mascota)
 }
 
+// getListPets godoc
+// @Summary  Obtiene el listado de mascotas
+// @ID       get-list-pet
+// @Produce  json
+// @Success   200 {object} []Pet
+// @Failure  405  not  allowed
+// @Router   /lismascotas [get]
 func getListPets(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(arrPet)
 }
 
-func main() {
+// @title        API - Mascota"
+// @version      1.0
+// @description  Microservicio con 3 endpoints:
 
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  MIT
+// @license.url   https://opensource.org/licenses/MIT
+
+// @host                     localhost:8022
+// @BasePath                 /
+// @query.collection.format  multi
+
+func main() {
 	router := mux.NewRouter()
 	port := ":8022"
 	server := &http.Server{
@@ -68,6 +110,8 @@ func main() {
 	router.HandleFunc("/creamascota", createPet).Methods("POST")
 	router.HandleFunc("/kpidemascotas", getKpiPet).Methods("GET")
 	router.HandleFunc("/lismascotas", getListPets).Methods("GET")
+	router.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger", http.FileServer(http.Dir("swagger"))))
+	router.PathPrefix("/docs/").Handler(http.StripPrefix("/docs", http.FileServer(http.Dir("docs"))))
 
 	log.Fatal(server.ListenAndServe())
 }
